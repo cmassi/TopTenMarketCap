f8599a4732d7ff8c685b8ecc2777d69882fc6f4e
Claudio Massi <c.massi@reply.it>
2024-11-28 19:06:11 +0100

    Initial Commit

---
+++ TopTenMarketCap/TopTenMarketCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenMarketCapApp.swift
+//  TopTenMarketCap
+//
+//  Created by Swift on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TopTenMarketCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TopTenMarketCapTests/TopTenMarketCapTests.swift
@@ -0,0 +1,16 @@
+//
+//  TopTenMarketCapTests.swift
+//  TopTenMarketCapTests
+//
+//  Created by Swift on 28/11/24.
+//
+
+import Testing
+
+struct TopTenMarketCapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
